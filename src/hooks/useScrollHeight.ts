8885a06a904f4506cdc83ba4680e5dc6d331f20d
Eepyamre <eepyamre@gmail.com>
2025-09-02 10:18:07 +0300

    Update useScrollHeight

--- src/hooks/useScrollHeight.ts
+++ src/hooks/useScrollHeight.ts
@@ -3,18 +3,26 @@ import { useEffect } from 'preact/hooks';
 export const useScrollHeight = (onBottomCallback: () => void) => {
   useEffect(() => {
     const fn = () => {
-      const { scrollTop, scrollHeight, clientHeight } =
-        document.documentElement;
+      const scrollTop =
+        window.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop ||
+        0;
+
+      const scrollHeight = document.documentElement.scrollHeight;
+      const clientHeight = window.innerHeight;
 
       if (scrollTop + clientHeight >= scrollHeight - 50) {
         onBottomCallback();
         window.removeEventListener('scroll', fn);
       }
     };
-    addEventListener('scroll', fn);
+
+    window.addEventListener('scroll', fn, { passive: true });
     fn();
+
     return () => {
-      removeEventListener('scroll', fn);
+      window.removeEventListener('scroll', fn);
     };
   }, []);
 };
